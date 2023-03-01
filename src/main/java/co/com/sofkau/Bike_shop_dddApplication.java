52b3f0c946ad5f7c2f1f0b4bdabd78e762f75fe1
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-03-01 14:46:13 -0500

    Spring boot dependencies and main application added

---
+++ src/main/java/co/com/sofkau/Bike_shop_dddApplication.java
@@ -0,0 +1,11 @@
+package co.com.sofkau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bike_shop_dddApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Bike_shop_dddApplication.class, args);
+    }
+}
